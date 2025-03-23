965342cf5065520d477e9fcd12757fe32d875b3e
marian <puubles@gmail.com>
2025-03-23 02:29:48 +0200

    swagger fixed for JWT

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,8 +7,18 @@ async function start() {
     openapi: {
       info: {
         title: "RedbJS API",
-        version: "0.1.0",
+        version: "1.0.0",
       },
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      },
+      security: [{ bearerAuth: [] }],
     },
   });
 
